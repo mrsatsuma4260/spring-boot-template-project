0b94971d805a4f69cc17641b5b5f2fbd881ed067
mrsatsuma4260 <mrsatsuma4260@gmail.com>
2024-02-03 15:06:07 +0300

    Add configuration

--- src/main/java/org/home/service/impl/MonitoringService.java
+++ src/main/java/org/home/service/impl/MonitoringService.java
@@ -1,12 +1,21 @@
 package org.home.service.impl;
 
+import org.home.properties.ApplicationProperties;
 import org.home.service.IMonitoringService;
+import org.springframework.stereotype.Service;
 
+@Service
 class MonitoringService
     implements IMonitoringService {
 
+    private final ApplicationProperties properties;
+
+    MonitoringService(final ApplicationProperties properties) {
+        this.properties = properties;
+    }
+
     @Override
     public String getVersion() {
-        return null;
+        return properties.getVersion();
     }
 }
